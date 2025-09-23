2a25e7076ed1e388057ef43c74eeb037d4c1ceee
Gaowsik <gowsick73@gmail.com>
2025-09-23 20:27:03 +0530

    Implemented user list fragment

--- app/src/main/java/com/example/assignmentuserlist/MainActivity.kt
+++ app/src/main/java/com/example/assignmentuserlist/MainActivity.kt
@@ -7,10 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.example.assignmentuserlist.presentation.core.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/assignmentuserlist/presentation/user/UserListFragment.kt
+++ app/src/main/java/com/example/assignmentuserlist/presentation/user/UserListFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.assignmentuserlist.R
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.assignmentuserlist.databinding.FragmentUserListBinding
+import com.example.assignmentuserlist.domain.User
 import com.example.assignmentuserlist.presentation.core.BaseFragment
+import com.example.assignmentuserlist.presentation.extention.collectLatestLifeCycleFlow
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UserListFragment : BaseFragment() {
 
     private lateinit var binding: FragmentUserListBinding
+    lateinit var navController: NavController
+    private val userViewModel: UserViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +32,78 @@ class UserListFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
+
+
     }
 
+    private fun init() {
+        navController = findNavController()
+        setTransactionRecycleView()
+        setUpObservers()
+        getUsers()
+
+    }
+
+    private fun setUpObservers() {
+
+        this.collectLatestLifeCycleFlow(userViewModel.errorMessage) {
+            showMessageDialogWithOkAction(it)
+        }
+
+        this.collectLatestLifeCycleFlow(userViewModel.userListState) {
+            setDataUserListAdapter(it)
+            binding.swipeRefreshLayout.isRefreshing = false
+
+        }
+
+        this.collectLatestLifeCycleFlow(userViewModel.isLoading) { isLoading ->
+            if (isLoading) {
+                showProgressDialog()
+            } else {
+                hideProgressDialog()
+            }
+        }
+
+        binding.editSearch.addTextChangedListener { input ->
+            binding.recyclerViewUserList?.adapter?.let {
+                (it as UserListAdapter).filter.filter(input)
+            }
+        }
+
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            getUsers()
+        }
+    }
+
+    private fun setDataUserListAdapter(userList: List<User>) {
+        binding.recyclerViewUserList.adapter?.let {
+            (it as UserListAdapter).setData(userList)
+        }
+    }
+
+    private fun setTransactionRecycleView() {
+        binding.recyclerViewUserList.layoutManager = LinearLayoutManager(context)
+        val userListRecycleAdapter = UserListAdapter { user ->
+            navigateToUserDetailScreen(user.userId)
+        }
+        binding.recyclerViewUserList.adapter = userListRecycleAdapter
+    }
+
+    private fun navigateToUserDetailScreen(
+        userId: String
+    ) {
+        val action =
+            UserListFragmentDirections.actionUserListFragmentToUserDetailFragment(
+                userId
+            )
+        navController.navigate(action)
+    }
+
+
+    private fun getUsers() {
+        userViewModel.getAllUsers()
+    }
 
 
 }

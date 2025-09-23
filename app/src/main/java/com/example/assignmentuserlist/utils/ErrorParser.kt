798e2d1a95c10e67ca87c35b6ab103d9cc1c2c6a
Gaowsik <gowsick73@gmail.com>
2025-09-23 12:46:20 +0530

    Implemented ErrorParser

---
+++ app/src/main/java/com/example/assignmentuserlist/utils/ErrorParser.kt
@@ -0,0 +1,52 @@
+package com.example.assignmentuserlist.utils
+
+import com.example.assignmentuserlist.data.APIResource
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+
+fun parseErrors(failure: APIResource.Error): String {
+    return when {
+        failure.isNetworkError -> "Network Error"
+
+        failure.errorCode == 403 -> {
+            "Unauthorized request"
+        }
+
+        failure.errorCode == 404 -> {
+            ("Resource not found")
+        }
+
+        failure.errorCode == 422 -> {
+            ("Validation error")
+        }
+
+        failure.errorCode == 500 -> {
+            try {
+                val errorBody = Gson().fromJson(failure.errorBody?.string(), JsonObject::class.java)
+                (errorBody.get("message").asString)
+            } catch (e: Exception) {
+                ("Internal server error")
+            }
+        }
+
+        failure.errorCode == 504 -> {
+            ("Gateway timeout")
+        }
+
+        failure.errorCode == 0 -> {
+            ("Unknown error")
+        }
+
+        else -> {
+            try {
+                val errorBody = failure.errorBody?.string()
+                val jsonObject = Gson().fromJson(errorBody, JsonObject::class.java)
+                val errorMessage = jsonObject.getAsJsonObject("error").get("message")?.asString
+                errorMessage ?: "Unknown error"
+            } catch (e: Exception) {
+                // Log the exception if needed
+                "Unknown error"
+            }
+        }
+    }
+}
